8d6e7ba5d2a3aa10cea6cb5d94ec5c5c2fb8cb2a
Brum <fbrump@gmail.com>
2016-05-04 18:57:44 -0300

    Subir o banco de dados em PostgreSQL
    Realizar ajustes futuros, estrutura das pastas e arquivos.

---
+++ vw_empregados_empresas.sql
@@ -0,0 +1,19 @@
+﻿CREATE OR REPLACE VIEW vw_empregados_empresas as
+select
+	e.id			as idEmpregado,
+	p.nome			as nmEmpregado,
+	p.sobrenome		as nmSobrenome,
+	c.nome			as nmEmpresa,
+	c.nome_fantasia		as nmEmpresaFantasia,
+	ef.nome			as nmFuncao,
+	ef.abreviacao		as dsFuncaoAbreviacao
+from
+	empregado 			as e
+	inner join pessoa 		as p
+		on p.id = e.id_pessoa
+	inner join empresa 		as c
+		on c.id = e.id_empresa
+	inner join empregado_funcao 	as ef
+		on ef.id = e.id_empregado_funcao
+order by
+	p.nome
