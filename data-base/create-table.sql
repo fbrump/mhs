6651c9402dc57a11c178593859d175ac61881b86
Brum <fbrump@gmail.com>
2016-05-05 01:10:52 -0300

    Atualização do script para criar as tabelas
    Removido alguns comentários desnecessários.

--- data-base/create-table.sql
+++ data-base/create-table.sql
@@ -10,13 +10,13 @@ CREATE TABLE pessoa(
 	data_nascimento date,
 	CONSTRAINT pk_pessoa PRIMARY KEY (id)
 );
--- drop table pessoa
 
 /* === INSERT ====*/
-insert into pessoa (nome, sobrenome, data_nascimento) values ('Rick', 'Fazollo', '04-01-1990');
-insert into pessoa (nome, sobrenome, data_nascimento) values ('Bruno', 'Silva', '1984-01-21');
-insert into pessoa (nome, sobrenome, data_nascimento) values ('Rafael', 'Carvalho', '1980-11-27');
-insert into pessoa (nome, sobrenome, data_nascimento) values ('Steve', 'Jobs', '1991-09-15');
+insert into pessoa (nome, sobrenome, data_nascimento) values ('Rick', 'Ventura Cordeiro', '1990-01-04');
+insert into pessoa (nome, sobrenome, data_nascimento) values ('Bruno', 'Silva Pereira', '1984-01-21');
+insert into pessoa (nome, sobrenome, data_nascimento) values ('Rafael', 'Carvalho dos Santos', '1980-11-27');
+insert into pessoa (nome, sobrenome, data_nascimento) values ('Steve', 'Jobs Kowalski', '1991-09-15');
+insert into pessoa (nome, sobrenome, data_nascimento) values ('Steve', 'Jobs Kowalski', '1991-12-01');
 
 /* =========================
 EMPRESA
@@ -28,6 +28,7 @@ create table empresa (
 	CONSTRAINT pk_empresa PRIMARY KEY (id)
 );
 
+/* === INSERT ====*/
 insert into empresa (nome, nome_fantasia) 
 values ('Thecnolgoy', 'T - Desenvolvimento e soluções de softwares');
 
@@ -41,6 +42,7 @@ create table empregado_funcao (
 	CONSTRAINT pk_empregado_funcao PRIMARY KEY (id)
 );
 
+/* === INSERT ====*/
 insert into empregado_funcao(nome,abreviacao) values('Desenvolvedor Júnior', 'Dev Jr');
 insert into empregado_funcao(nome,abreviacao) values('Desenvolvedor Pleno', 'Dev Pl');
 insert into empregado_funcao(nome,abreviacao) values('Desenvolvedor Senior', 'Dev Sr');
@@ -63,29 +65,13 @@ create table empregado (
 	CONSTRAINT uq_empregado_key UNIQUE(id_empresa, id_pessoa, id_empregado_funcao)
 );
 
+/* === INSERT ====*/
 insert into empregado (id_empresa, id_pessoa, id_empregado_funcao) values(1, 1, 2);
 insert into empregado (id_empresa, id_pessoa, id_empregado_funcao) values(1, 2, 1);
 insert into empregado (id_empresa, id_pessoa, id_empregado_funcao) values(1, 3, 3);
 insert into empregado (id_empresa, id_pessoa, id_empregado_funcao) values(1, 4, 5);
 
-
-/*
-pessoa
-- nome
-- sobrenome
-- data_nascimento
-
-empresa
-- nome
-- apelido
-
-
-empregado
-- id_pessoa
-- id_funcao
-- id_empresa
-*/
-
+/* === SELECT MODULO ====*/
 select * from pessoa;
 select * from empresa;
 select * from empregado_funcao;
@@ -106,6 +92,7 @@ create table folha_ponto_status (
 	CONSTRAINT uq_folha_ponto_status UNIQUE(nome)
 );
 
+/* === INSERT ====*/
 insert into folha_ponto_status (nome) values ('Criada');
 insert into folha_ponto_status (nome) values ('Alimentando');
 insert into folha_ponto_status (nome) values ('Validando');
@@ -128,6 +115,7 @@ create table folha_ponto(
 	CONSTRAINT uq_folha_ponto UNIQUE(id_empregado,mes, ano)
 );
 
+/* === INSERT ====*/
 insert into folha_ponto (id_empregado, id_folha_ponto_status, mes, ano) values (1, 1,01, 2015);
 insert into folha_ponto (id_empregado, id_folha_ponto_status, mes, ano, carga_horaria) values (1, 1,02, 2015, '08:00:00');
 
@@ -143,6 +131,7 @@ create table dia_semana (
 	CONSTRAINT uq_dia_semana UNIQUE(nome, sigla)
 );
 
+/* === INSERT ====*/
 insert into dia_semana(nome, sigla) values ('Domingo', 'Dom');
 insert into dia_semana(nome, sigla) values ('Segunda-feira', 'Seg');
 insert into dia_semana(nome, sigla) values ('Terça-feira', 'Ter');
@@ -181,6 +170,7 @@ create table anexo_tipo (
 	CONSTRAINT uq_anexo_tipo UNIQUE(nome)
 );
 
+/* === INSERT ====*/
 insert into anexo_tipo (nome) values ('Atestado Médico');
 insert into anexo_tipo (nome) values ('Atestado Dentista');
 
@@ -211,6 +201,7 @@ create table lancamento_horas_anexo (
 	CONSTRAINT uq_lancamento_horas_anexo UNIQUE(id_lancamento_horas, id_anexo)
 );
 
+/* === SELECT MODULO ====*/
 select * from lancamento_horas_anexo;
 select * from anexo_tipo;
 select * from anexo;  
@@ -218,40 +209,6 @@ select * from dia_semana;
 select * from folha_ponto_status;
 select * from folha_ponto;
 select * from lancamento_horas;
-
-/*
-folha_ponto
-- id_empregado
-- mês
-- ano
-- carga_horaria
-
-lancamento_horas
-- dia_mes
-- dia_semana
-- hora_entrada
-- hora_entreda_intervalo
-- hora_saida_intervalo
-- hora_saida
-- flag_dia_descanso
-- flag_feriado
-
-lancamento_horas_anexo
-- id_lancamento_horas
-- id_anexo
-- id_anexo_tipo
-
-anexo_tipo
-- id
-- nome
-
-anexo
-- id
-- nome
-- descricao
-- link
-*/
-
 
 /* =========================
 ==== LIMPAR A BASE
